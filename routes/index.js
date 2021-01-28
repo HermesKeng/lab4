898a694e8cddd6b604e0831787e1a636f353ed3c
HermesKeng <kaining1101@gmail.com>
2021-01-28 00:32:59 -0800

    nodejs lab

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
-
 /*
  * GET home page.
  */
 
-exports.view = function(req, res){
-  res.render('index');
+exports.view = function (req, res) {
+  res.render("index", {
+    projects: [
+      {
+        name: "Waiting in Line",
+        image: "lorempixel.abstract.1.jpeg",
+        id: "project1",
+        url: "project/Waiting in Line",
+      },
+      {
+        name: "Needfinding",
+        image: "lorempixel.city.1.jpeg",
+        id: "project2",
+        url: "project/Needfinding",
+      },
+      {
+        name: "Nice life",
+        image: "lorempixel.city.2.jpeg",
+        id: "project3",
+        url: "project/Nice life",
+      },
+    ],
+  });
 };
---
+++ routes/project.js
@@ -0,0 +1,6 @@
+exports.view = function (req, res) {
+  var name = req.params.name;
+  res.render("project", {
+    name,
+  });
+};
